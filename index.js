925edc227f5981c4d80bb176f723f55e13fcaf77
Zev <rakha2728@gmail.com>
2024-08-02 21:23:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const app = express();
+const path = require("path");
 
 mongoose
   .connect("mongodb://localhost/notes")
@@ -25,3 +26,11 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+
+app.use("/css", express.static(path.join(__dirname, "node_modules/@fortawesome/fontawesome-free/css")));
+
+app.use("/webfonts", express.static(path.join(__dirname, "node_modules/@fortawesome/fontawesome-free/webfonts")));
